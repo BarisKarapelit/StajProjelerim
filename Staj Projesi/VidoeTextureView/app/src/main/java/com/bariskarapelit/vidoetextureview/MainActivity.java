5c5e6f642b5b0400f7cc69421d7601fc5223fda6
Barış Karapelit <baris_karapelit_298@hotmail.com>
2020-08-05 16:44:49 +0300

    Twilio Kütüphanesini implemetation ve ViewView

--- Staj Projesi/VidoeTextureView/app/src/main/java/com/bariskarapelit/vidoetextureview/MainActivity.java
+++ Staj Projesi/VidoeTextureView/app/src/main/java/com/bariskarapelit/vidoetextureview/MainActivity.java
@@ -3,12 +3,27 @@ package com.bariskarapelit.vidoetextureview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+import com.twilio.video.CameraCapturer;
+import com.twilio.video.LocalAudioTrack;
+import com.twilio.video.LocalVideoTrack;
+import com.twilio.video.VideoRenderer;
+
+public class MainActivity extends AppCompatActivity
+{
+    VideoView videoView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
     }
 }
