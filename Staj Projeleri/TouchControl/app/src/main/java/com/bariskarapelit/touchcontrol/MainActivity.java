85b0d6c5775582088ae3a41a46a9734d0dce626a
Barış Karapelit <baris_karapelit_298@hotmail.com>
2020-08-29 18:35:26 +0300

    23.günün raporu

--- Staj Projeleri/TouchControl/app/src/main/java/com/bariskarapelit/touchcontrol/MainActivity.java
+++ Staj Projeleri/TouchControl/app/src/main/java/com/bariskarapelit/touchcontrol/MainActivity.java
@@ -25,69 +25,94 @@ public class MainActivity extends AppCompatActivity
     ImageView imageView ;
 
 
-    LinearLayout root;
+    int selectedImage = R.drawable.circlepng;
+
+
+
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = new ImageView(this);
 
-        FrameLayout frameLayout = findViewById(R.id.frame_layout);
+
+
+
+
+
+        //Burası defult resim. Butonlara tıklamadğında ilk olarak varsayılan olarka koaycağınız resim olacak.
+        imageView.setImageResource(R.drawable.circlepng);
+
+
+
+        final FrameLayout frameLayout = findViewById(R.id.frame_layout);
         LinearLayout linearLayout= findViewById(R.id.linearlayout);
 
         circleButton=findViewById(R.id.circle);
         dislikeButton=findViewById(R.id.dislike);
         likeButton=findViewById(R.id.like);
 
-        root = findViewById(R.id.linearlayout);
-        MultiTouchControl multiTouchControl = new MultiTouchControl(root, new MultiTouchControl.ComponentView() {
+        videoTextureView=findViewById(R.id.video_view_top_right);
+
+
+
+
+        //Burdaki hangi viewi dinleyeceği hocam.
+        MultiTouchControl multiTouchControl = new MultiTouchControl(frameLayout, new MultiTouchControl.ComponentView() {
             @Override
             public View onCreateComponent() {
-                imageView = new ImageView(root.getContext());
-                
-                circleButton.setOnClickListener(new View.OnClickListener()
-                {
-                    @Override
-                    public void onClick(View view)
-                    {
-                        Toast.makeText(MainActivity.this,"Circle",Toast.LENGTH_LONG).show();
-                        imageView.setImageResource(R.drawable.circlepng);
+                ImageView image = new ImageView(frameLayout.getContext());
+                image.setImageResource(selectedImage);
 
-
-                    }
-                });
-
-                dislikeButton.setOnClickListener(new View.OnClickListener()
-                {
-                    @Override
-                    public void onClick(View view)
-                    {
-                        Toast.makeText(MainActivity.this,"Dislike",Toast.LENGTH_LONG).show();
-                        imageView.setImageResource(R.drawable.circlepng);
-
-
-                    }
-                });
-                likeButton.setOnClickListener(new View.OnClickListener()
-                {
-                    @Override
-                    public void onClick(View view)
-                    {
-                        Toast.makeText(MainActivity.this,"Like",Toast.LENGTH_LONG).show();
-
-                        imageView.setImageResource(R.drawable.circlepng);
-
-                    }
-                });
-
-                return imageView;
+                return image;
             }
         });
 
         multiTouchControl.startListener();
 
-
-
+        setupButtons();
     }
+
+    private void setupButtons(){
+        circleButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(MainActivity.this,"Circle",Toast.LENGTH_LONG).show();
+              selectedImage = R.drawable.circlepng;
+
+
+            }
+        });
+
+        dislikeButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(MainActivity.this,"Dislike",Toast.LENGTH_LONG).show();
+                selectedImage = R.drawable.dislike;
+
+
+
+            }
+        });
+        likeButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(MainActivity.this,"Like",Toast.LENGTH_LONG).show();
+
+                selectedImage = R.drawable.like;
+
+
+            }
+        });
+    }
+
+
 }
